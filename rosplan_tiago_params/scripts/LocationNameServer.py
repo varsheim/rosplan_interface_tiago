d23574e60bbeea98a2802510c39dfdfb410c35fe
varsheim <lzielinski66@gmail.com>
2019-04-21 14:13:09 +0200

    json config is read

--- rosplan_tiago_params/scripts/LocationNameServer.py
+++ rosplan_tiago_params/scripts/LocationNameServer.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python
 
+import json
+from rospkg import rospack
 from rosplan_tiago_params.srv import *
 import rospy
 from geometry_msgs.msg import Pose, Point, Quaternion
 
+config_path = '/home/luke/tiago_rosplan_ws/src/rosplan_interface_tiago/rosplan_tiago_params/config/location.json'
+
 
 def get_pose_by_name(name, config):
+    with open(config_path) as json_data:
+        d = json.load(json_data)
+        json_data.close()
+        print d
+
     point = Point(1, 1, 1)
     quat = Quaternion(0, 0, 0, 1)
 
